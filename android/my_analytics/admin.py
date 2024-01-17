487cc221f8bd63e3be5e660d40532dbc57841680
RFradioFactory <nevstruev.2004@mail.ru>
2024-01-18 02:18:44 +0500

    Update admin.py

--- android/my_analytics/admin.py
+++ android/my_analytics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Navigation)
+admin.site.register(Relevance)
+admin.site.register(Location)
+admin.site.register(Abilities)
+admin.site.register(HeadHunterLV)
+admin.site.register(MainPage)
